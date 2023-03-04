e65f76f8cd9a595e9696ff0f36ceebfb81fb6bcf
Kuro1Fury <skycoder0820@gmail.com>
2023-03-03 16:44:28 -0800

    create admin

--- user_labelling/main/admin.py
+++ user_labelling/main/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from .models import Label, Item
+
+admin.site.register(Label)
+admin.site.register(Item)
